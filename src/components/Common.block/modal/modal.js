92480e1fed5e61589a35ea233f1fc7c6901f5529
sivilenok <cherepahasivi@gmail.com>
2022-08-31 20:51:27 +0200

    add modal.js

---
+++ src/components/Common.block/modal/modal.js
@@ -0,0 +1,19 @@
+const modalBtnConfirm = document.querySelector('.modal__bttn');
+modalBtnConfirm.addEventListener('click', renderConfirm);
+
+export const renderEdit = () => {
+    const modalWrapper = document.querySelector('.modal__wrapper');
+    modalWrapper.style.display = "block";
+}
+
+export const renderDelete = () => {
+    const modalWrapper = document.querySelector('')
+}
+
+export const renderConfirm = () => {
+    const modalWrapper = document.querySelector('.modal__wrapper');
+    modalWrapper.style.display = "none";
+}
+
+
+
