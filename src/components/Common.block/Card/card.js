cd59eb3831caea90891af88cac78e4fe7e75b2e8
pyropetrick <pavel9719@tut.by>
2022-08-31 21:55:53 +0300

    add styles

--- src/components/Common.block/Card/card.js
+++ src/components/Common.block/Card/card.js
@@ -1,89 +1,107 @@
 import{editItem, deleteItem , renderConfirm} from '../modal/modal'
 let btnAddTodo = document.querySelector('.item-todo__button-add-todo');
+const todos = [];
 
-function renderTodo() {
+function addTodo() {
+    let date = new Date();
+    let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+    }
+    let todo = {
+        id: Date.now(),
+        title: '',
+        description: '',
+        time: date.toLocaleString('ru', options)
+    }
+
+    todos.push(todo);
+    renderTodo();
+}
+function renderTodo(list = todos) {
     const tasksList = document.querySelector('.tasks-list');
-    // task
-    const task = document.createElement('li');
-    task.classList.add('tasks-list__item');
+    tasksList.innerHTML = '';
+    list.forEach(todo => {
+        const task = document.createElement('li');
+        task.classList.add('tasks-list__item');
 
-    // titles 
-    const titlesBlock = document.createElement('div');
-    titlesBlock.classList.add('tasks-list__item-titles');
-    
-    // title 
+        // titles 
+        const titlesBlock = document.createElement('div');
+        titlesBlock.classList.add('tasks-list__item-titles');
+        
+        // title 
 
-    const title = document.createElement('p');
-    title.classList.add('tasks-list__item-title');
-    title.innerText = 'Title';
+        const title = document.createElement('p');
+        title.classList.add('tasks-list__item-title');
+        title.innerText = 'Title';
 
-    // desc
+        // desc
 
-    const desc = document.createElement('p');
-    desc.classList.add('tasks-list__item-desc');
-    desc.innerText = 'Description';
+        const desc = document.createElement('p');
+        desc.classList.add('tasks-list__item-desc');
+        desc.innerText = 'Description';
 
-    // user 
-    const user = document.createElement('p');
-    user.classList.add('tasks-list__item-user');
-    user.innerText = 'User';
+        // user 
+        const user = document.createElement('p');
+        user.classList.add('tasks-list__item-user');
+        user.innerText = 'User';
 
-    // actions 
+        // actions 
 
-    const actionsBlock = document.createElement('div');
-    actionsBlock.classList.add('tasks-list__item-actions');
+        const actionsBlock = document.createElement('div');
+        actionsBlock.classList.add('tasks-list__item-actions');
 
-    // editing block
+        // editing block
 
-    const editBlock = document.createElement('div');
-    editBlock.classList.add('tasks-list__item-actions-editing');
+        const editBlock = document.createElement('div');
+        editBlock.classList.add('tasks-list__item-actions-editing');
 
-    // buttons
+        // buttons
 
-    const btnEdit = document.createElement('button');
-    btnEdit.classList.add('tasks-list__item-actions-edit');
-    btnEdit.innerHTML = 'Edit';
-    btnEdit.addEventListener('click', editItem);
+        const btnEdit = document.createElement('button');
+        btnEdit.classList.add('tasks-list__item-actions-edit');
+        btnEdit.innerHTML = 'Edit';
+        btnEdit.addEventListener('click', editItem);
 
-    const btnDelete = document.createElement('button');
-    btnDelete.classList.add('tasks-list__item-actions-delete');
-    btnDelete.innerHTML = 'Delete';
-    // btnDelete.addEventListener('click', deleteItem);
+        const btnDelete = document.createElement('button');
+        btnDelete.classList.add('tasks-list__item-actions-delete');
+        btnDelete.innerHTML = 'Delete';
 
-    const btnJump = document.createElement('button');
-    btnJump.classList.add('tasks-list__item-actions-jump');
-    btnJump.innerHTML = '>';
+        const btnJump = document.createElement('button');
+        btnJump.classList.add('tasks-list__item-actions-jump');
+        btnJump.innerHTML = '>';
 
-    // time 
+        // time 
 
-    const time = document.createElement('p');
-    time.classList.add('tasks-list__item-actions-time');
-    time.innerHTML = 'Time';
+        const time = document.createElement('p');
+        time.classList.add('tasks-list__item-actions-time');
+        time.innerHTML = todo.time;
 
-    // composite to list
+        // composite to list
 
-    // titles 
-    titlesBlock.append(title);
-    titlesBlock.append(desc);
-    titlesBlock.append(user);
+        // titles 
+        titlesBlock.append(title);
+        titlesBlock.append(desc);
+        titlesBlock.append(user);
 
-    // actions 
+        // actions 
 
-    editBlock.append(btnEdit);
-    editBlock.append(btnDelete);
-    actionsBlock.append(editBlock);
-    actionsBlock.append(btnJump);
-    actionsBlock.append(time);
-    
-    // task 
-    task.append(titlesBlock);
-    task.append(actionsBlock);
+        editBlock.append(btnEdit);
+        editBlock.append(btnDelete);
+        actionsBlock.append(editBlock);
+        actionsBlock.append(btnJump);
+        actionsBlock.append(time);
+        
+        // task 
+        task.append(titlesBlock);
+        task.append(actionsBlock);
 
-    // list 
-    tasksList.append(task);
+        // list 
+        tasksList.append(task);
+        })// task
 
 }
 
-btnAddTodo.addEventListener('click', renderTodo);
+btnAddTodo.addEventListener('click', addTodo);
 
 
