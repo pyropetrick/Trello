1d13739bcb1033fd80da0923d7f047d3679e18f6
sivilenok <cherepahasivi@gmail.com>
2022-09-02 20:46:00 +0200

    add function deliteItem

--- src/components/Common.block/Card/card.js
+++ src/components/Common.block/Card/card.js
@@ -1,4 +1,4 @@
-import{renderEdit, renderDelete, renderConfirm} from '../modal/modal'
+import{editItem, deleteItem , renderConfirm} from '../modal/modal'
 let btnAddTodo = document.querySelector('.item-todo__button-add-todo');
 
 function renderTodo() {
@@ -43,12 +43,12 @@ function renderTodo() {
     const btnEdit = document.createElement('button');
     btnEdit.classList.add('tasks-list__item-actions-edit');
     btnEdit.innerHTML = 'Edit';
-    btnEdit.addEventListener('click', renderEdit);
+    btnEdit.addEventListener('click', editItem);
 
     const btnDelete = document.createElement('button');
     btnDelete.classList.add('tasks-list__item-actions-delete');
     btnDelete.innerHTML = 'Delete';
-    btnDelete.addEventListener('click', renderDelete);
+    btnDelete.addEventListener('click', deleteItem);
 
     const btnJump = document.createElement('button');
     btnJump.classList.add('tasks-list__item-actions-jump');
--- src/components/Common.block/modal/modal.js
+++ src/components/Common.block/modal/modal.js
@@ -1,43 +1,28 @@
 import { addToStorage, removeFromStorage } from "../../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Frontend/TMS/JS/Lesson 11/todo-list-vanilla/scripts/storageHelpers";
 
-let tasks = [];
-let IDX = 0;
-const UNIQUE_ID_PREFIX = 'Tasks-List_';
-
-
 const modalBtnConfirm = document.querySelector('.modal__bttn');
 const modalBtnCancel = document.querySelector('.modal__btn');
+const modalWrapper = document.querySelector('.modal__wrapper');
+let todos = [];
+let IDX = 0;
 
-export const renderEdit = () => {
-    const modalWrapper = document.querySelector('.modal__wrapper');
-    modalWrapper.style.display = "block";
+export const editItem = () => {
+    modalWrapper.style.display = "flex";
 }
 
-export const renderDelete = ({ target }) => {
-   if (target.classList.contains('tasks-list__item-actions-delete')) {
-      let taskUIndex = target.dataset.taskIndex;
-      const taskIndex = tasks.findIndex(({ id }) => === taskUIndex);
-      if (taskIndex !== -1) {
-        tasks.splice(taskIndex, 1);
-
-        if (tasks.length) {
-            addToStorage({ key: 'tasks', value: tasks});
-        } else {
-            removeFromStorage('tasks')
-;       }
-        renderTodos();
-        renderCounters();
-      }
-   }
+export const deleteItem = (event) => {
+    if (event.target.classList.contains('tasks-list__item-actions-delete')) {
+        let 
+    }     
+    })
+    
 }
 
 export const renderConfirm = () => {
-    const modalWrapper = document.querySelector('.modal__wrapper');
     modalWrapper.style.display = "none";
 }
 
 export const renderCancel = () => {
-    const modalWrapper = document.querySelector('.modal__wrapper');
     modalWrapper.style.display = "none";
 }
 
