16c0d83ddce31cf4949a203c4e9a0fc4edccb7b6
pyropetrick <100130236+pyropetrick@users.noreply.github.com>
2022-09-10 11:08:40 +0000

    Deploying to gh-pages from @ pyropetrick/Trello@3f97b9d50610508686231550ba7a074fb0bebb48 🚀

---
+++ index.befea99a.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{new Promise(((e,o)=>{fetch("https://jsonplaceholder.typicode.com/users").then((o=>{o.ok&&e(o.json())})).catch((e=>o(e)))})).then((e=>function(e){console.log(e),e.forEach((({id:e,name:o})=>{console.log(e,o)}))}(e))).catch((e=>new Error(e)))}));
+//# sourceMappingURL=index.befea99a.js.map
